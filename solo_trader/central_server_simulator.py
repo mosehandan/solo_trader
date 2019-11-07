0d265e0ff1e543220e0e78f424de3996cf172b80
Honfung Wong <21543236+onshek@users.noreply.github.com>
2019-11-07 18:02:42 +0800

    [update] several details

--- solo_trader/central_server_simulator.py
+++ solo_trader/central_server_simulator.py
@@ -3,11 +3,12 @@
 """
 
 # import pprint
-from solo_trader.data_center_simulator import DataCenter
+import time
 import socket
 import pickle
 import threading
 import socketserver
+from solo_trader.data_center_simulator import DataCenter
 
 
 class Handler(socketserver.BaseRequestHandler):
@@ -16,14 +17,17 @@ class Handler(socketserver.BaseRequestHandler):
         print('Successful Access')
         print('Records Are Below')
         print('=================')
+        data_center = DataCenter(init_price=100.00)
         while True:
             data = self.request.recv(1024)
             if data == b'break':
                 print('=================')
                 break
             print(pickle.loads(data))
+            # print(data_center.get_data())
             self.request.send(b'continue')
-            continue
+            time.sleep(0.1)
+            # continue
 
 
 class Server(object):
@@ -39,40 +43,36 @@ class Server(object):
         print('Server Closed')
 
 
-class Trader(object):
+class Client(object):
 
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
-        self.orders = self.get_order()
-
-    @staticmethod
-    def get_order():
-        return [1, 2, 3]
+        self.content = list(range(10))
 
     def run(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         _ = s.connect((self.ip, self.port))
         while True:
             try:
-                content = self.orders.pop()
+                content = self.content.pop()
             except IndexError:
                 s.send(b'break')
                 s.close()
                 break
             _ = s.send(pickle.dumps(content))
             response = s.recv(1024)
-            if response == b'continue':
-                continue
+            # if response == b'continue':
+            #     continue
 
 
 if __name__ == "__main__":
-    server = Server('localhost', 9998)
-    trader = Trader('localhost', 9998)
-    thread1 = threading.Thread(target=server.run)
-    thread2 = threading.Thread(target=trader.run)
+    server_simulator = Server('localhost', 9998)
+    client_simulator = Client('localhost', 9998)
+    thread1 = threading.Thread(target=server_simulator.run)
+    thread2 = threading.Thread(target=client_simulator.run)
     thread1.start()
     thread2.start()
     thread2.join()
-    server.stop()
+    server_simulator.stop()
     thread1.join()
--- solo_trader/data_center_simulator.py
+++ solo_trader/data_center_simulator.py
@@ -10,8 +10,7 @@ import pandas as pd
 
 class DataCenter:
 
-    def __init__(self,
-                 init_price: float = 100.000):
+    def __init__(self, init_price: float = 100.000):
         """
         初始化
         :param init_price: （发行）开盘价
@@ -25,9 +24,9 @@ class DataCenter:
         # 新增数据
         self.new_data = {}
 
-    def generate_tick_data(self):
+    def generate_tick_data(self) -> None:
         """
-        模拟一个 tick 盘口挂单数据更新
+        模拟一个 tick 盘口挂单数据更新，围绕 init_price 生成随机价格和数量
         :return: None
         """
         tmp_price = np.random.randn() // 0.01 / 100
@@ -39,14 +38,28 @@ class DataCenter:
         }
 
     @staticmethod
-    def _append(existing_data, new_data, ascending):
+    def _append(existing_data: pd.DataFrame,
+                new_data: dict,
+                ascending: bool or None) -> pd.DataFrame:
+        """
+        工具函数，将 generate_tick_data 生成的新数据，加入已有的数据（pd.DataFrame）
+        :param existing_data: 已有的数据（pd.DataFrame）
+        :param new_data: 新数据
+        :param ascending: 分买盘和卖盘，针对 price 排序
+        :return: 合并完成的数据
+        """
         if ascending is None:
             return existing_data.append(pd.DataFrame({k: [new_data[k]] for k in new_data})).reset_index(drop=True)
         else:
             return existing_data.append(pd.DataFrame({k: [new_data[k]] for k in new_data})).sort_values(
                 by="price", ascending=ascending).reset_index(drop=True)
 
-    def match_tick_data(self):
+    def match_tick_data(self) -> None:
+        """
+        撮合引擎
+        注意：成交价格由主动方价格决定
+        :return: None
+        """
         if self.new_data["direction"] == 1:  # 买方挂单
             available_data = self.sell_data[self.sell_data["price"] < self.new_data["price"]]
             if len(available_data) == 0:  # 卖方对手盘挂单价格过高，无法成交
@@ -61,7 +74,7 @@ class DataCenter:
                         else:  # 卖方数量不足
                             self.new_data["quantity"] -= available_data.loc[idx, "quantity"]  # 买方挂单减去相应数量
                             self.sell_data.drop(index=idx, axis=0, inplace=True)  # 删除卖方挂单
-                        print(record_data)
+                        # print(record_data)
                         self.history_data = self._append(self.history_data, record_data, ascending=None)  # 成交记录进入历史成交
                 if self.new_data["quantity"] > 0:
                     self.buy_data = self._append(self.buy_data, self.new_data, ascending=False)  # 买方挂单进入买盘
@@ -79,10 +92,26 @@ class DataCenter:
                         else:  # 买方数量不足
                             self.new_data["quantity"] -= available_data.loc[idx, "quantity"]  # 卖方挂单减去相应数量
                             self.buy_data.drop(index=idx, axis=0, inplace=True)  # 删除卖方挂单
-                        print(record_data)
+                        # print(record_data)
                         self.history_data = self._append(self.history_data, record_data, ascending=None)  # 成交记录进入历史成交
                 if self.new_data["quantity"] > 0:
                     self.sell_data = self._append(self.sell_data, self.new_data, ascending=True)  # 卖方挂单进入买盘
+
+    def get_data(self) -> dict:
+        """
+        执行 generate_tick_data 和 match_tick_data，并且返回 trade、quote、order 信息
+        :return:
+        """
+        self.generate_tick_data()
+        self.match_tick_data()
+        trade_data = self.history_data.iloc[-1] if len(self.history_data) > 0 else None
+        quote_data = self.sell_data.append(self.buy_data, ignore_index=True).reset_index(drop=True)
+        order_data = None
+        return {
+            "trade_data": trade_data,
+            "quote_data": quote_data.to_dict(),
+            "order_data": order_data.to_dict()
+        }
 
 
 if __name__ == "__main__":
